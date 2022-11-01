9cce255c3a42083227de99d9c2a45f0e42f7cadd
ADIL SHIEKH <114225192+Darkranger00@users.noreply.github.com>
2022-11-01 05:01:14 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+// const { constants } = require("buffer")
+
+// console.log("always availabe")
+// //var a = "being human"
+// //console.log(a)
+// let a = "being human"
+// a = 4
+// //a = "heeey"
+// //console.log(a)
+// {
+//   let a = "it's me"
+//   console.log(a)
+// }
+// console.log(a)
+// const author = "This is code written by aadil"
+// console.log(author)
+
+// let p = null
+// let q = 876;
+// const item = {
+//   "aadil": null,
+//   "miss": false,
+//   "harry": "it's me",
+//   "meri": 65,
+//   "rohan": undefined
+// } 
+// console.log(item["aadil"], "miss")
+
+// // questions 
+// let indel = "me as a co-piolet"
+// let indell = 6
+// console.log(indell + indell)
+// console.log(65*indell)
+
+// console.log(typeof (indell + indel))
+// const Database = require("@replit/database")
+
+/* let comp1 = 9;
+let comp2 = 8;
+console.log("console", comp1 == comp2)
+console.log("console", comp1 != comp2)
+console.log("console", comp1 === comp2) //for checking of string and number */
+
+// let be = prompt("hii adil is here what's your age")
+// if (be>9) {
+//   alert("we are happy to see you")
+// }
+// else if (be<9){
+//   alert("you seem like bacha")
+// }
+
+
